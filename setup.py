def437148a114f66d1ca30bf2398a017002b2cd6
Foster McLane <fkmclane@gmail.com>
2017-09-25 16:39:09 -0400

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ video = Extension(
 
 setup(
 	name='ardrone',
-	version='0.2',
+	version='0.2.1',
 	description='A Python library for controlling the Parrot AR.Drone 2.0 over a network',
+	url='https://github.com/fkmclane/python-ardrone',
 	license='MIT',
 	author='Foster McLane',
 	author_email='fkmclane@gmail.com',
