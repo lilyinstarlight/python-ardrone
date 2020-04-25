2e5cf435f188f3f0783c19b4de86eb75507df31d
Foster McLane <fkmclane@gmail.com>
2020-04-25 09:17:35 -0400

    move atcmd upkeep into ardrone.at and only create a single command socket per logical drone

--- ardrone/at.py
+++ ardrone/at.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import threading
 
 import ardrone.constant
 
@@ -13,143 +14,155 @@ def f2i(f):
     return struct.unpack('i', struct.pack('f', f))[0]
 
 
-def ref(host, seq, takeoff, emergency=False):
-    """
-    Basic behaviour of the drone: take-off/landing, emergency stop/reset)
+class ATCommand(object):
+    def __init__(self, host):
+        """
+        Open a new AT command socket
 
-    Parameters:
-    seq -- sequence number
-    takeoff -- True: Takeoff / False: Land
-    emergency -- True: Turn off the engines
-    """
-    p = 0b10001010101000000000000000000
-    if takeoff:
-        p |= 0b1000000000
-    if emergency:
-        p |= 0b100000000
-    at(host, 'REF', seq, [p])
+        Parameters:
+        host -- destination address
+        """
+        self.host = host
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+        self.seq = 1
+        self.interval = 0.2
 
-def pcmd(host, seq, progressive, lr, fb, vv, va):
-    """
-    Makes the drone move (translate/rotate).
+        self.comwdg_timer = threading.Timer(self.interval, self.comwdg)
+        self.lock = threading.Lock()
 
-    Parameters:
-    seq -- sequence number
-    progressive -- True: enable progressive commands, False: disable (i.e.
-        enable hovering mode)
-    lr -- left-right tilt: float [-1..1] negative: left, positive: right
-    rb -- front-back tilt: float [-1..1] negative: forwards, positive:
-        backwards
-    vv -- vertical speed: float [-1..1] negative: go down, positive: rise
-    va -- angular speed: float [-1..1] negative: spin left, positive: spin
-        right
+    def halt(self):
+        """
+        Halts communication with the drone
+        """
+        self.comwdg_timer.cancel()
 
-    The above float values are a percentage of the maximum speed.
-    """
-    p = 1 if progressive else 0
-    at(host, 'PCMD', seq, [p, float(lr), float(fb), float(vv), float(va)])
+    def ref(self, takeoff, emergency=False):
+        """
+        Basic behaviour of the drone: take-off/landing, emergency stop/reset)
 
+        Parameters:
+        takeoff -- True: Takeoff / False: Land
+        emergency -- True: Turn off the engines
+        """
+        p = 0b10001010101000000000000000000
+        if takeoff:
+            p |= 0b1000000000
+        if emergency:
+            p |= 0b100000000
+        self.at('REF', [p])
 
-def ftrim(host, seq):
-    """
-    Tell the drone it's lying horizontally.
+    def pcmd(self, progressive, lr, fb, vv, va):
+        """
+        Makes the drone move (translate/rotate).
 
-    Parameters:
-    seq -- sequence number
-    """
-    at(host, 'FTRIM', seq, [])
+        Parameters:
+        progressive -- True: enable progressive commands, False: disable (i.e.
+            enable hovering mode)
+        lr -- left-right tilt: float [-1..1] negative: left, positive: right
+        rb -- front-back tilt: float [-1..1] negative: forwards, positive:
+            backwards
+        vv -- vertical speed: float [-1..1] negative: go down, positive: rise
+        va -- angular speed: float [-1..1] negative: spin left, positive: spin
+            right
 
+        The above float values are a percentage of the maximum speed.
+        """
+        p = 1 if progressive else 0
+        self.at('PCMD', [p, float(lr), float(fb), float(vv), float(va)])
 
-def zap(host, seq, stream):
-    """
-    Selects which video stream to send on the video UDP port.
+    def ftrim(self):
+        """
+        Tell the drone it's lying horizontally.
+        """
+        self.at('FTRIM')
 
-    Parameters:
-    seq -- sequence number
-    stream -- Integer: video stream to broadcast
-    """
-    # FIXME: improve parameters to select the modes directly
-    at(host, 'ZAP', seq, [stream])
+    def zap(self, stream):
+        """
+        Selects which video stream to send on the video UDP port.
 
+        Parameters:
+        stream -- Integer: video stream to broadcast
+        """
+        # FIXME: improve parameters to select the modes directly
+        self.at('ZAP', [stream])
 
-def config(host, seq, option, value):
-    """Set configuration parameters of the drone."""
-    at(host, 'CONFIG', seq, [str(option), str(value)])
+    def config(self, option, value):
+        """Set configuration parameters of the drone."""
+        self.at('CONFIG', [str(option), str(value)])
 
+    def comwdg(self):
+        """
+        Reset communication watchdog.
+        """
+        self.at('COMWDG')
 
-def comwdg(host, seq):
-    """
-    Reset communication watchdog.
-    """
-    # FIXME: no sequence number
-    at(host, 'COMWDG', seq, [])
+    def aflight(self, flag):
+        """
+        Makes the drone fly autonomously.
 
+        Parameters:
+        flag -- Integer: 1: start flight, 0: stop flight
+        """
+        self.at('AFLIGHT', [flag])
 
-def aflight(host, seq, flag):
-    """
-    Makes the drone fly autonomously.
+    def pwm(self, m1, m2, m3, m4):
+        """
+        Sends control values directly to the engines, overriding control loops.
 
-    Parameters:
-    seq -- sequence number
-    flag -- Integer: 1: start flight, 0: stop flight
-    """
-    at(host, 'AFLIGHT', seq, [flag])
+        Parameters:
+        m1 -- Integer: front left command
+        m2 -- Integer: front right command
+        m3 -- Integer: back right command
+        m4 -- Integer: back left command
+        """
+        self.at('PWM', [m1, m2, m3, m4])
 
+    def led(self, anim, f, d):
+        """
+        Control the drones LED.
 
-def pwm(host, seq, m1, m2, m3, m4):
-    """
-    Sends control values directly to the engines, overriding control loops.
+        Parameters:
+        anim -- Integer: animation to play
+        f -- Float: frequency in HZ of the animation
+        d -- Integer: total duration in seconds of the animation
+        """
+        self.at('LED', [anim, float(f), d])
 
-    Parameters:
-    seq -- sequence number
-    m1 -- Integer: front left command
-    m2 -- Integer: front right command
-    m3 -- Integer: back right command
-    m4 -- Integer: back left command
-    """
-    at(host, 'PWM', seq, [m1, m2, m3, m4])
+    def anim(self, anim, d):
+        """
+        Makes the drone execute a predefined movement (animation).
 
+        Parameters:
+        anim -- Integer: animation to play
+        d -- Integer: total duration in seconds of the animation
+        """
+        self.at('ANIM', [anim, d])
 
-def led(host, seq, anim, f, d):
-    """
-    Control the drones LED.
+    def at(self, command, params=[]):
+        """
+        Encodes and sends AT command
 
-    Parameters:
-    seq -- sequence number
-    anim -- Integer: animation to play
-    f -- Float: frequency in HZ of the animation
-    d -- Integer: total duration in seconds of the animation
-    """
-    at(host, 'LED', seq, [anim, float(f), d])
+        Parameters:
+        command -- the command
+        params -- a list of elements which can be either int, float or string
+        """
+        params_str = []
+        for p in params:
+            if type(p) == int:
+                params_str.append('{:d}'.format(p))
+            elif type(p) == float:
+                params_str.append('{:d}'.format(f2i(p)))
+            elif type(p) == str:
+                params_str.append('"{:s}"'.format(p))
 
+        with self.lock:
+            self.comwdg_timer.cancel()
 
-def anim(host, seq, anim, d):
-    """
-    Makes the drone execute a predefined movement (animation).
+            msg = 'AT*{:s}={:d}{:s}\r'.format(command, self.seq, ''.join(',' + param for param in params_str))
+            self.sock.sendto(msg.encode(), (self.host, ardrone.constant.COMMAND_PORT))
 
-    Parameters:
-    seq -- sequcence number
-    anim -- Integer: animation to play
-    d -- Integer: total duration in seconds of the animation
-    """
-    at(host, 'ANIM', seq, [anim, d])
+            self.seq += 1
 
-def at(host, command, seq, params):
-    """
-    Parameters:
-    command -- the command
-    seq -- the sequence number
-    params -- a list of elements which can be either int, float or string
-    """
-    params_str = []
-    for p in params:
-        if type(p) == int:
-            params_str.append('{:d}'.format(p))
-        elif type(p) == float:
-            params_str.append('{:d}'.format(f2i(p)))
-        elif type(p) == str:
-            params_str.append('"{:s}"'.format(p))
-    msg = 'AT*{:s}={:d},{:s}\r'.format(command, seq, ','.join(params_str))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(msg.encode(), (host, ardrone.constant.COMMAND_PORT))
+            self.comwdg_timer = threading.Timer(self.interval, self.comwdg)
+            self.comwdg_timer.start()
--- ardrone/drone.py
+++ ardrone/drone.py
@@ -3,7 +3,6 @@ Python library for the AR.Drone.
 """
 
 import time
-import threading
 import multiprocessing
 
 import PIL.Image
@@ -22,13 +21,11 @@ class ARDrone(object):
     def __init__(self, host='192.168.1.1'):
         self.host = host
 
-        self.sequence = 1
-        self.timer = 0.2
-        self.com_watchdog_timer = threading.Timer(self.timer, self.commwdg)
-        self.lock = threading.Lock()
         self.speed = 0.2
-        self.at(ardrone.at.config, 'general:navdata_demo', 'TRUE')
-        self.at(ardrone.at.config, 'control:altitude_max', '20000')
+
+        self.atcmd = ardrone.at.ATCommand()
+        self.atcmd.config('general:navdata_demo', 'TRUE')
+        self.atcmd.config('control:altitude_max', '20000')
         self.video_pipe, video_pipe_other = multiprocessing.Pipe()
         self.nav_pipe, nav_pipe_other = multiprocessing.Pipe()
         self.com_pipe, com_pipe_other = multiprocessing.Pipe()
@@ -36,70 +33,72 @@ class ARDrone(object):
         self.network_process.start()
         self.ipc_thread = ardrone.network.IPCThread(self)
         self.ipc_thread.start()
+
         self.image = PIL.Image.new('RGB', (640, 360))
         self.navdata = dict()
+
         self.time = 0
 
     def takeoff(self):
         """Make the drone takeoff."""
-        self.at(ardrone.at.ref, True)
+        self.atcmd.ref(True)
 
     def land(self):
         """Make the drone land."""
-        self.at(ardrone.at.ref, False)
+        self.atcmd.ref(False)
 
     def hover(self):
         """Make the drone hover."""
-        self.at(ardrone.at.pcmd, False, 0, 0, 0, 0)
+        self.atcmd.pcmd(False, 0, 0, 0, 0)
 
     def move_left(self):
         """Make the drone move left."""
-        self.at(ardrone.at.pcmd, True, -self.speed, 0, 0, 0)
+        self.atcmd.pcmd(True, -self.speed, 0, 0, 0)
 
     def move_right(self):
         """Make the drone move right."""
-        self.at(ardrone.at.pcmd, True, self.speed, 0, 0, 0)
+        self.atcmd.pcmd(True, self.speed, 0, 0, 0)
 
     def move_up(self):
         """Make the drone rise upwards."""
-        self.at(ardrone.at.pcmd, True, 0, 0, self.speed, 0)
+        self.atcmd.pcmd(True, 0, 0, self.speed, 0)
 
     def move_down(self):
         """Make the drone decent downwards."""
-        self.at(ardrone.at.pcmd, True, 0, 0, -self.speed, 0)
+        self.atcmd.pcmd(True, 0, 0, -self.speed, 0)
 
     def move_forward(self):
         """Make the drone move forward."""
-        self.at(ardrone.at.pcmd, True, 0, -self.speed, 0, 0)
+        self.atcmd.pcmd(True, 0, -self.speed, 0, 0)
 
     def move_backward(self):
         """Make the drone move backwards."""
-        self.at(ardrone.at.pcmd, True, 0, self.speed, 0, 0)
+        self.atcmd.pcmd(True, 0, self.speed, 0, 0)
 
     def turn_left(self):
         """Make the drone rotate left."""
-        self.at(ardrone.at.pcmd, True, 0, 0, 0, -self.speed)
+        self.atcmd.pcmd(True, 0, 0, 0, -self.speed)
 
     def turn_right(self):
         """Make the drone rotate right."""
-        self.at(ardrone.at.pcmd, True, 0, 0, 0, self.speed)
+        self.atcmd.pcmd(True, 0, 0, 0, self.speed)
 
     def reset(self):
         """Toggle the drone's emergency state."""
-        self.at(ardrone.at.ref, False, True)
+        self.atcmd.ref(False, True)
         time.sleep(0.1)
-        self.at(ardrone.at.ref, False, False)
+        self.atcmd.ref(False, False)
 
     def trim(self):
         """Flat trim the drone."""
-        self.at(ardrone.at.ftrim)
+        self.atcmd.ftrim
 
     def set_cam(self, cam):
         """Set active camera.
 
         Valid values are 0 for the front camera and 1 for the bottom camera
         """
-        self.at(ardrone.at.config, 'video:video_channel', cam)
+        self.atcmd.config('video:video_channel', cam)
 
     def set_speed(self, speed):
         """Set the drone's speed.
@@ -107,28 +106,6 @@ class ARDrone(object):
         Valid values are floats from [0..1]
         """
         self.speed = speed
-
-    def at(self, cmd, *args, **kwargs):
-        """Wrapper for the low level at commands.
-
-        This method takes care that the sequence number is increased after each
-        at command and the watchdog timer is started to make sure the drone
-        receives a command at least every second.
-        """
-        with self.lock:
-            self.com_watchdog_timer.cancel()
-            cmd(self.host, self.sequence, *args, **kwargs)
-            self.sequence += 1
-            self.com_watchdog_timer = threading.Timer(self.timer, self.commwdg)
-            self.com_watchdog_timer.start()
-
-    def commwdg(self):
-        """Communication watchdog signal.
-
-        This needs to be sent regularly to keep the communication
-        with the drone alive.
-        """
-        self.at(ardrone.at.comwdg)
 
     def halt(self):
         """Shutdown the drone.
@@ -139,7 +116,7 @@ class ARDrone(object):
         with this object.
         """
         with self.lock:
-            self.com_watchdog_timer.cancel()
+            self.atcmd.halt()
             self.ipc_thread.stop()
             self.ipc_thread.join()
             self.network_process.terminate()
@@ -155,4 +132,4 @@ class ARDrone(object):
         vv -- vertical speed: float [-1..1] negative: go down, positive: rise
         va -- angular speed: float [-1..1] negative: spin left, positive: spin
             right"""
-        self.at(ardrone.at.pcmd, True, lr, fb, vv, va)
+        self.atcmd.pcmd(True, lr, fb, vv, va)
